603a57526a320ea2151a4cc73f3558e05feaacd1
JuanchoGonzalez <juancruzg30@gmail.com>
2025-10-08 09:30:39 -0300

    correcciones chiquitas

--- assembler/ver.c
+++ assembler/ver.c
@@ -1,4 +1,4 @@
-#include <ver.h>
+#include "ver.h"
 
 void generar_codigo_assembler(codigo3dir programa[], FILE *out) {
     if (!out) return;
--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 #include "analisis-semantico/semantico.h"
 #include "utils/manejo_errores.h"
 #include "codigo-intermedio/generador.h"
-#include "assemblerrr/ver.h"
+#include "assembler/ver.h"
 
 #define COLOR_RED     "\033[31m"
 #define COLOR_GREEN   "\033[32m"
